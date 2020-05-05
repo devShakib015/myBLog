8e98f609202a3e1e161001f30c7672679d84ee76
shakib <kmshahriahhossain@gmail.com>
2020-05-06 03:14:10 +0600

    ready blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    published_posts = Post.objects.filter(status='published')
+    context = {
+        'posts': posts,
+        'published_posts': published_posts,
+    }
+    return render(request, 'blog/index.html', context)
+
+def post_detail(request,posts):
+    post = get_object_or_404(Post, slug=posts)
+    context = {'post': post}
+    return render(request, 'blog/post_detail.html', context)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+STATIC_URL = '/static/'
 
 TEMPLATES = [
     {
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
