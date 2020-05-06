7cd1b868432f4610bae75a09834fb6246e88da0b
shakib <kmshahriahhossain@gmail.com>
2020-05-06 14:50:12 +0600

    Update upto Generic

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.index, name='index'),
+    path('blog/', views.PostListView.as_view(), name='post_list'),
     path('<slug:posts>/', views.post_detail, name='post_detail')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.all()
-    published_posts = Post.objects.filter(status='published')
-    context = {
-        'posts': posts,
-        'published_posts': published_posts,
-    }
-    return render(request, 'blog/index.html', context)
+def index(request):
+    return render(request, 'blog/index.html')
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(status='published')
+    paginate_by = 3
+    context_object_name = 'p_p'
+    template_name = 'blog/post_list.html'
 
 def post_detail(request,posts):
     post = get_object_or_404(Post, slug=posts)
