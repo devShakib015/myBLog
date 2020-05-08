8a2a8d7b7ba382442a9d850ee577efcb5a93c332
shakib <kmshahriahhossain@gmail.com>
2020-05-08 21:35:24 +0600

    Updated upto Comment

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
@@ -18,7 +18,21 @@ class PostListView(ListView):
 
 def post_detail(request,posts):
     post = get_object_or_404(Post, slug=posts)
-    context = {'post': post}
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    context = {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form}
     return render(request, 'blog/post_detail.html', context)
 
 def post_share(request, post_id):
