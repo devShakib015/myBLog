6e1aee620b21fdc881c9dc057735fb3aa970ac66
shakib <kmshahriahhossain@gmail.com>
2020-05-17 21:09:39 +0600

    nothing

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,6 +33,7 @@ SITE_ID = 1
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'account.apps.AccountConfig',
     'taggit',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -129,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # SMTP Gmail
